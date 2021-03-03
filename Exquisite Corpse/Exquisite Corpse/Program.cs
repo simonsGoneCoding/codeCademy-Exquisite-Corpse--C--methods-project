057af6e59638f9cf26c8c05e76edf6263451ddb5
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-03-03 18:22:20 +0000

    Initial commit - codeCademy - mehtods project in C#

---
+++ Exquisite Corpse/Exquisite Corpse/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ExquisiteCorpse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void GhostHead()
+        {
+            Console.WriteLine("     ..-..");
+            Console.WriteLine("    ( o o )");
+            Console.WriteLine("    |  O  |");
+        }
+
+        static void GhostBody()
+        {
+            Console.WriteLine("    |     |");
+            Console.WriteLine("    |     |");
+            Console.WriteLine("    |     |");
+        }
+
+        static void GhostFeet()
+        {
+            Console.WriteLine("    |     |");
+            Console.WriteLine("    |     |");
+            Console.WriteLine("    '~~~~~'");
+        }
+
+        static void BugHead()
+        {
+            Console.WriteLine("     /   \\");
+            Console.WriteLine("     \\. ./");
+            Console.WriteLine("    (o + o)");
+        }
+
+        static void BugBody()
+        {
+            Console.WriteLine("  --|  |  |--");
+            Console.WriteLine("  --|  |  |--");
+            Console.WriteLine("  --|  |  |--");
+        }
+
+        static void BugFeet()
+        {
+            Console.WriteLine("     v   v");
+            Console.WriteLine("     *****");
+        }
+
+        static void MonsterHead()
+        {
+            Console.WriteLine("     _____");
+            Console.WriteLine(" .-,;='';_),-.");
+            Console.WriteLine("  \\_\\(),()/_/");
+            Console.WriteLine("　  (,___,)");
+        }
+
+        static void MonsterBody()
+        {
+            Console.WriteLine("   ,-/`~`\\-,___");
+            Console.WriteLine("  / /).:.('--._)");
+            Console.WriteLine(" {_[ (_,_)");
+        }
+
+        static void MonsterFeet()
+        {
+            Console.WriteLine("    |  Y  |");
+            Console.WriteLine("   /   |   \\");
+            Console.WriteLine("   \"\"\"\" \"\"\"\"");
+        }
+    }
+}
